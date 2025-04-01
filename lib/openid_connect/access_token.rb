e89b75c4e9816e285e73072e2f728175036ed98d
ad-mcas <137486137+ad-mcas@users.noreply.github.com>
2025-04-01 21:34:03 +0200

    Update access_token.rb

--- lib/openid_connect/access_token.rb
+++ lib/openid_connect/access_token.rb
@@ -29,8 +29,8 @@ module OpenIDConnect
       case res.status
       when 200
         if res.headers.has_key?('content-type') and res.headers['content-type'].start_with?('application/jwt')
-          raise "error #{res.to_hash}"
-          JSON::JWT.decode(res.body, :skip_verification).with_indifferent_access
+          decoded = JSON::JWT.decode(res.body, :skip_verification)
+          decoded.with_indifferent_access
         else
           res.body.with_indifferent_access
         end
